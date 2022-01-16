ee9e3f6b9a28977b21ec598144985eba59edecbb
Văn Bang <toanhocolympic@gmail.com>
2022-01-16 10:49:47 +0000

    .

--- js/object.js
+++ js/object.js
@@ -1,4 +1,31 @@
 // Website: https://javascript.info/object
+
+/* Bản tóm tắt
+Đối tượng là các mảng liên kết với một số tính năng đặc biệt.
+
+Chúng lưu trữ các thuộc tính (cặp khóa-giá trị), trong đó:
+
+Khóa thuộc tính phải là chuỗi hoặc ký hiệu (thường là chuỗi).
+Giá trị có thể thuộc bất kỳ loại nào.
+Để truy cập một thuộc tính, chúng tôi có thể sử dụng:
+
+obj.property Kí hiệu dấu chấm:.
+Kí hiệu dấu ngoặc vuông obj["property"]. Dấu ngoặc vuông cho phép lấy khóa từ một biến, như obj[varWithKey].
+Các toán tử bổ sung:
+
+Để xóa một thuộc tính delete obj.prop:.
+Để kiểm tra xem thuộc tính có khóa đã cho có tồn tại "key" in objhay không:.
+Để lặp qua một đối tượng: for (let key in obj) vòng lặp.
+Những gì chúng ta đã nghiên cứu trong chương này được gọi là "đối tượng đơn giản", hay chỉ Object.
+
+Có nhiều loại đối tượng khác trong JavaScript:
+
+Array để lưu trữ các bộ sưu tập dữ liệu có thứ tự,
+Date để lưu trữ thông tin về ngày và giờ,
+Error để lưu trữ thông tin về một lỗi.
+…Và như thế.
+*/
+
 // An empty object(“empty cabinet”) can be created using one of two syntaxes:
 
 // let user = new Object(); // "object constructor" syntax
@@ -22,9 +49,14 @@ let name = {
     age: 20,
     address: 'BR- VT',
     'Email Edu': 'bangnguyen.31201023793@st.ueh.edu.vn',
+    fullName: function () {
+        return this.firstName + ' Van ' + this.lastName;
+    }
 };
 
 console.log(name);
+console.log(name.fullName);
+console.log(name.fullName());
 
 // Giá trị có thể thuộc bất kỳ loại nào. Hãy thêm một boolean:
 
@@ -55,12 +87,132 @@ console.log(user[key]); // John (if enter "name")
 let fruit = 'apple';
 
 let bag = {
-  [fruit]: 5, // the name of the property is taken from the variable fruit
+    [fruit]: 5, // the name of the property is taken from the variable fruit
 };
 
 // console.log( bag.apple ); // 5 if fruit="apple"
 
 bag[fruit + 'Computers'] = 'free';
-console.log(bag)
+console.log(bag);
+
+
+// Property value shorthand
+function makeUser(name, age) {
+    return {
+        name: name,
+        age: age,
+        // ...other properties
+    };
+}
+
+let infoUser = makeUser("John", 30);
+
+console.log(infoUser);
+
+
+// Giới hạn tên thuộc tính
+// Như chúng ta đã biết, một biến không thể có tên bằng một trong các từ dành riêng cho ngôn ngữ như “for”, “let”, “return”, v.v.
+// Nhưng đối với thuộc tính đối tượng, không có hạn chế như vậy:
+
+// these properties are all right
+let obj = {
+    for: 1,
+    let: 2,
+    return: 3
+};
+
+console.log(obj.for + obj.let + obj.return);  // 6
+
+// Kiểm tra thuộc tính có tồn tại hay không
+let information = {
+    telephone: null
+}
+
+console.log(information.name === undefined);
+console.log('telephone' in information)
+// Cú pháp là:  "key" in object
+// Nếu chúng ta bỏ qua dấu ngoặc kép, điều đó có nghĩa là một biến, nó phải chứa tên thực cần được kiểm tra
+console.log(`Kiem tra xem co ton tai hay khong: `, 'lastName' in name);
+
+// Vòng lặp
+// Cú pháp:
+
+// for (key in object) {
+//     // executes the body for each key among object properties
+// }
+
+for (let key in name) {
+    console.log(`${key}: `, name[key]);
+}
+
+for (let prop in name) {
+    console.log(prop);
+}
+
+function isEmpty(obj) {
+    let number = 0;
+    for (let key in obj) {
+        number++;
+    }
+
+    let result = number !== 0 ? true : false;
+    return result;
+}
+
+/* Cách hay hơn
+function isEmpty(obj) {
+  for (let key in obj) {
+    // if the loop has started, there is a property
+    return false;
+  }
+  return true;
+}
+*/
+
+let info = {};
+
+// info.name = 'Van Bang';
+
+console.log(isEmpty(info));
+console.log(isEmpty(name));
+
+// Tính tổng số lượng
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+}
+
+function sum(obj) {
+    let result = 0;
+    for (let key in obj) {
+        result += obj[key];
+    }
+    return result;
+}
+
+console.log(sum(salaries));
+
+// Nhân các thuộc tính là số với 2
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+};
+
+function multiflyNumeric(obj) {
+    for (let key in obj) {
+        if (typeof (obj[key]) == 'number') {
+            obj[key] = obj[key] * 2;
+        }
+    }
+    return obj;
+}
+
+console.log(multiflyNumeric(menu));
+
+
+
+
 
 
---
+++ js/string.js
@@ -0,0 +1,204 @@
+// Khai báo 
+let answer1 = "It's alright";
+let answer2 = "He is called 'Johnny'";
+let answer3 = 'He is called "Johnny"';
+// Để tìm độ dài chuỗi ta dùng hàm length
+console.log(answer1.length)
+// JavaScript Template Literals
+let guestList = `Guests:
+ * John
+ * Pete
+ * Mary
+`;
+console.log(guestList); // a list of guests, multiple lines
+
+let text = `He's often called "Johnny"`;
+
+let firstName = "John";
+let lastName = "Doe";
+let name = `Welcome ${firstName}, ${lastName}!`;
+console.log(name);
+
+
+// Dùng (\)  để biến thành ký tự chuỗi 
+// Example
+
+let = text = "We are the so-called \"Vikings\" from the north.";
+console.log(text); //We are the so-called "Vikings" from the north.
+
+// Chuỗi \\ sẽ giúp viết ký tự \ trong chuỗi
+let text1 = "The character \\ is called backslash.";
+console.log(text1); //The character \ is called backslash.
+
+// “ký tự dòng mới”, được viết dưới dạng \n, biểu thị dấu ngắt dòng:
+let text5 = 'hello my name \n is bang';
+console.log(text5); //hello my name 
+// is bang
+
+// Chuỗi dưới dạng đối tượng 
+let y = 'Hello';
+console.log(typeof(y)); // string
+let x = new String('Hello');
+let z = new String('Hello');
+console.log(x); 
+console.log(typeof(x)); //object
+
+console.log(x == y); //true
+console.log(x === y); //false
+console.log(x == z); //false
+console.log(x === z); //false
+
+// => không nên tạo đối tượng chuỗi
+
+// -------------------------String Method---------------------
+/* Trích xuất chuỗi
+có 3 phương pháp trích xuất chuỗi:
+        slice(start, end)
+        substring(start, end)
+        substr(start, length)
+*/
+
+/* String slice 
+slice() extracts a part of a string and returns the extracted part in a new string.
+The method takes 2 parameters: the start position, and the end position (end not included).
+*/
+// vị trí start là vị trí bắt đầu cắt, và end là vị trí muốn cắt đến
+let fullName = 'Nguyen van bang';
+let slice = fullName.slice(-8);
+console.log(slice); // van
+// First position is 0
+// Second position is 1
+// Nếu giá trị âm thì lấy từ phải qua trái
+// Nếu end ở đây không có tham số nghia là lấy hết
+
+/* Substring
+substring() tương tự như slice().
+Sự khác biệt là substring()không thể chấp nhận các chỉ số âm.
+*/
+
+let address = "Ba ria vung tau va dep tau";
+console.log(address.substring(7, 9));
+
+/* String substr() 
+substr() tương tự như slice().
+Sự khác biệt là tham số thứ hai chỉ định độ dài của phần được trích xuất.
+*/
+console.log(address.substr(7, 9))
+console.log(address.substr(6))
+console.log(address.substr(-3,2))
+
+/* String replace method
+Phương replace() thức thay thế một giá trị được chỉ định bằng một giá trị khác trong một chuỗi:
+*/
+
+console.log(address.replace('tau', 'sinh'))
+// Ghi chú
+// Phương replace() thức không thay đổi chuỗi mà nó được gọi.
+// Phương replace() thức trả về một chuỗi mới.
+// Theo mặc định, replace() phương thức chỉ thay thế kết quả phù hợp đầu tiên
+// Để thay đổi tât cả ta sẽ dùng biểu thức chính quy /g, và khi dùng ta sẽ bỏ dấu ngoặc kép
+console.log(address.replace(/tau/g, 'sinh'))
+// Phương thức replace sẽ phân biệt chữ hoa và chữ thường để không phân biệt ta sẽ dùng biểu thức chính quy /i
+console.log(address.replace(/Tau/i, 'xinh'))
+
+/* Viết hoa và viết thường 
+Một chuỗi được chuyển đổi thành chữ hoa với toUpperCase():
+Một chuỗi được chuyển đổi thành chữ thường với toLowerCase():
+*/
+console.log(address.toLocaleLowerCase());
+console.log(address.toLocaleUpperCase());
+
+/* JavaScript String concat ()
+concat() nối hai hoặc nhiều chuỗi: 
+Phương concat()thức này có thể được sử dụng thay cho toán tử cộng. 
+*/
+console.log(address.concat(' - ', answer2));
+
+/* JavaScript String trim ()
+Phương trim()thức loại bỏ khoảng trắng từ cả hai bên của một chuỗi
+*/
+
+let ex1 = '        hello world        ';
+console.log(ex1.trim());
+
+// Các phương pháp giải nén ký tự chuỗi
+/* There are 3 methods for extracting string characters:
+        charAt(position)
+        charCodeAt(position)
+        Property access [ ]
+*/
+
+// Phương charAt() thức trả về ký tự tại một chỉ mục (vị trí) được chỉ định trong một chuỗi
+console.log(fullName.charAt(4))
+
+/* Phương thức charCodeAt() 
+trả về mã unicode của ký tự tại một chỉ mục được chỉ định trong một chuỗi:
+Phương thức này trả về mã UTF-16 (một số nguyên từ 0 đến 65535).
+*/
+console.log(fullName.charCodeAt(3));
+
+// ECMAScript 5 (2009) cho phép truy cập thuộc tính [] trên các chuỗi:
+console.log(fullName[9])
+console.log(fullName[9] == 'a');
+
+// Converting a String to an Array
+// Một chuỗi có thể được chuyển đổi thành một mảng bằng split()phương thức
+console.log(fullName.split(''));
+console.log(fullName.split(' '));
+
+/* JavaScript String Search
+    String indexOf()
+    String lastIndexOf()
+    String startsWith()
+    String endsWith()
+*/
+
+console.log(fullName.length);
+// Phương thức indexOf() trả về chỉ số của (vị trí của) sự xuất hiện của một văn bản được chỉ định trong một chuỗi:
+console.log(fullName.indexOf('n'));
+// Phương thức lastIndexOf() trả về chỉ mục của lần xuất hiện cuối cùng của một văn bản được chỉ định trong một chuỗi
+console.log(fullName.lastIndexOf('n'));
+// Cả hai indexOf() và lastIndexOf() trả về -1 nếu không tìm thấy văn bản
+console.log(fullName.indexOf('i'));
+console.log(fullName.lastIndexOf('i'));
+// Cả hai phương thức đều chấp nhận tham số thứ hai làm vị trí bắt đầu cho tìm kiếm
+console.log(fullName.indexOf('n',6)); // tìm từ trái sang phải
+console.log(fullName.lastIndexOf('n', 4)); // tìm từ phải sang trái
+
+// Phương thức search() tìm kiếm một chuỗi cho một giá trị được chỉ định và trả về vị trí của kết quả khớp
+console.log(fullName.search(/N/i));
+
+// So sánh giữa indexOf and search
+/*
+search() không thể nhận đối số vị trí bắt đầu thứ hai.
+indexOf() không thể nhận các giá trị tìm kiếm mạnh mẽ (biểu thức chính quy).
+*/
+// Phương thức match () tìm kiếm một chuỗi để so khớp với một biểu thức chính quy và trả về các kết quả phù hợp dưới dạng một đối tượng Array.
+let ex2 = "The rain in SPAIN stays mainly in the plain";
+console.log(ex2.match(/ain/g));
+// Nếu một biểu thức chính quy không bao gồm công cụ sửa đổi g (để thực hiện tìm kiếm toàn cục ), thì phương thức match () sẽ chỉ trả về kết quả phù hợp đầu tiên trong chuỗi.
+console.log(ex2.match(/AIN/i));
+console.log(ex2.match(/AIN/gi));
+
+// Phương includes()thức trả về true nếu một chuỗi chứa một giá trị được chỉ định.
+/* Cú pháp
+string.includes(searchvalue, start)
+searchvalue     	Required. The string to search for
+start	            Optional. Default 0. Position to start the search
+Returns:	        Returns true if the string contains the value, otherwise false */
+
+console.log(fullName.includes('n', 14) === false); // nếu tìm thấy sẽ trả về true
+
+// Phương startsWith()thức trả về true nếu một chuỗi bắt đầu bằng một giá trị được chỉ định, nếu không false:
+// string.startsWith(searchvalue, start)
+console.log(fullName.startsWith('n'));
+console.log(fullName.startsWith('n', 5));
+
+// Phương endsWith()thức trả về true nếu một chuỗi kết thúc bằng một giá trị được chỉ định, nếu không false
+// string.endswith(searchvalue, length)
+console.log(fullName.endsWith('g'));
+// Kiểm tra 3 ký tự đầu tiên của một chuỗi kết thúc bằng "u"
+console.log(fullName.endsWith('u', 3));
+
+
+
